b8b88dac169e07481818c9defb42cbacbb92b494
azkarmoulana <azkar.moulana@gmail.com>
2019-11-02 14:37:43 +0530

    main todo js comp

---
+++ src/Components/Todos.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+
+    render() {
+        return this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} markCompleted={this.props.markComplete} removeTask={this.props.removeTask}/>
+        ));
+    }
+}
+
+// Prop Types
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+export default Todos;
