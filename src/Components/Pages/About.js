63471f3a94cf5b0fd45b009d5d3be5052a477599
azkarmoulana <azkar.moulana@gmail.com>
2019-11-02 14:38:07 +0530

    about comp

---
+++ src/Components/Pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1 style={{ textAlign: 'center'}}>About ToDo</h1>
+            <h4 style={{ textAlign: 'center'}}>V 1.0.0</h4>
+            <p style={{ padding: '10px', color: 'grey'}}>This is a simple ToDO apppitlcation which helps you to organize your day today tasks.</p>
+        </React.Fragment>
+    )
+}
+
+export default  About;
